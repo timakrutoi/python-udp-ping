9803fc217f1a210c897f56a7ef4a58d946f38bd5
Timofei <timaoleinik@gmail.com>
2022-12-13 01:44:27 +0300

    Flake8

--- ping.py
+++ ping.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 #encode=utf-8
 
-import select
 import socket
 import struct
 import time
@@ -21,11 +20,19 @@ def dns_lookup(hostname):
     return socket.gethostbyname(hostname)
 
 
-def ping(hostname, dest_port, port, packet_size, number_of_pings, sleep_time, timeout):
+def ping(
+        hostname,
+        dest_port, port, packet_size, number_of_pings,
+        sleep_time, timeout):
+
     sock_out = socket.socket(
-            family=socket.AF_INET, type=socket.SOCK_RAW, proto=socket.IPPROTO_UDP)
+            family=socket.AF_INET,
+            type=socket.SOCK_RAW,
+            proto=socket.IPPROTO_UDP)
     sock_in = socket.socket(
-            family=socket.AF_INET, type=socket.SOCK_RAW, proto=socket.IPPROTO_ICMP)
+            family=socket.AF_INET,
+            type=socket.SOCK_RAW,
+            proto=socket.IPPROTO_ICMP)
 
     sock_in.settimeout(timeout)
 
@@ -61,17 +68,20 @@ def ping(hostname, dest_port, port, packet_size, number_of_pings, sleep_time, ti
 
             times.append(round((time.time() - s)*1000, 2))
             print(f'recieved reply in {times[-1]}ms, '
-                f'code {rec_pkg[ip_hdr_size]}, type {rec_pkg[ip_hdr_size + 1]}')
+                  f'(ICMP code {rec_pkg[ip_hdr_size]}, '
+                  f'type {rec_pkg[ip_hdr_size + 1]})')
 
             time.sleep(sleep_time / 1000)
 
         except KeyboardInterrupt:
             break
 
+    print()
     if len(times) > 0:
-        print(f'\nTotal {len(times)} pings with avg time {round(sum(times) / len(times), 2)}ms.')
+        print(f'Total {len(times)} pings '
+              f'with avg time {round(sum(times) / len(times), 2)}ms.')
     else:
-        print(f'\nNo successful pings.')
+        print('No successful pings.')
 
 
 if __name__ == '__main__':
@@ -79,19 +89,19 @@ if __name__ == '__main__':
 
     p = ArgumentParser()
     p.add_argument('--ip', '--hostname', dest='hostname', type=str,
-        help='IP of server to ping.')
+                   help='IP of server to ping.')
     p.add_argument('--dest-port', type=int, default=4567,
-        help='Port to get reply. Default = %(default)d.')
+                   help='Port to get reply. Default = %(default)d.')
     p.add_argument('--port', type=int, default=33434,
-        help='Port to ping. Default = %(default)d.')
+                   help='Port to ping. Default = %(default)d.')
     p.add_argument('-l', '--packet-size', type=int, default=64,
-        help='Size of packet. Default = %(default)d.')
+                   help='Size of packet. Default = %(default)d.')
     p.add_argument('-n', '--number-of-pings', type=int, default=3,
-        help='Number of pings. Default = %(default)d.')
+                   help='Number of pings. Default = %(default)d.')
     p.add_argument('--sleep-time', type=int, default=500,
-        help='Time between pings in ms. Default = %(default)d.')
-    p.add_argument('--timeout', type=int , default=10,
-        help='Time to wait for reply in s. Default = %(default)f.')
+                   help='Time between pings in ms. Default = %(default)d.')
+    p.add_argument('--timeout', type=int, default=10,
+                   help='Time to wait for reply in s. Default = %(default)f.')
 
     a = p.parse_args()
 
